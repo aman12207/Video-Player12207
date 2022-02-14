367eb46d21abdccd225a2183b328e15f84f486b1
aman <amangupta12207@gmail.com>
2022-02-14 16:52:33 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import SearchBar from './SearchBar'
 import youtube from '../api/youtube'
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
-import VideoItem from './VideoItem';
 
 class App extends React.Component{
   state = {videos : [] ,selectedVideo :null };
